2fe623ad49d3d4e8583c3a0a3440aaa4b7298849
Heidi <heidi.cone@clearpoint.co.nz>
2024-07-04 16:07:40 +1200

    clear out unused code

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 import { ChatAnthropic } from "@langchain/anthropic";
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
-// import { StringOutputParser } from "@langchain/core/output_parsers";
-// import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { VoyageEmbeddings } from "@langchain/community/embeddings/voyage";
-// import { Document } from "@langchain/core/documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-// import { MessagesPlaceholder } from "@langchain/core/prompts";
 import { HumanMessage, AIMessage } from "@langchain/core/messages";
 
 const chatModel = new ChatAnthropic({});
-
-// // var answer = await chatModel.invoke("what is LangSmith?");
-
-// // console.log(answer);
-
-// const prompt = ChatPromptTemplate.fromMessages([
-//   ["system", "You are a world class technical documentation writer."],
-//   ["user", "{input}"],
-// ]);
-
-// const chain = prompt.pipe(chatModel);
-
-// var answer = await chain.invoke({
-//   input: "what is LangSmith?",
-// });
-
-// console.log(answer);
-
-// const outputParser = new StringOutputParser();
-
-// const llmChain = prompt.pipe(chatModel).pipe(outputParser);
-
-// var answer = await llmChain.invoke({
-//   input: "what is LangSmith?",
-// });
-
-// console.log(answer);
 
 const loader = new CheerioWebBaseLoader(
   "https://lilianweng.github.io/posts/2023-06-23-agent/"
@@ -48,28 +17,11 @@ const loader = new CheerioWebBaseLoader(
 
 const docs = await loader.load();
 
-// console.log(docs.length);
-// console.log(docs[0].pageContent.length);
-
-// const textSplitter = new RecursiveCharacterTextSplitter({
-//   chunkSize: 1000,
-//   chunkOverlap: 200,
-// });
-
-// const splits = await textSplitter.splitDocuments(docs);
-
-// console.log(splits.length);
-// console.log(splits[0].pageContent);
 
 const embeddings = new VoyageEmbeddings({
   apiKey: process.env.VOYAGEAI_API_KEY, 
 });
 
-
-// const vectorStore = await MemoryVectorStore.fromDocuments(
-//   splits,
-//   embeddings
-// );
 
 const splitter = new RecursiveCharacterTextSplitter();
 
@@ -94,17 +46,6 @@ const documentChain = await createStuffDocumentsChain({
   prompt,
 });
 
-// await documentChain.invoke({
-//   input: "what is LangSmith?",
-//   context: [
-//     new Document({
-//       pageContent:
-//         "LangSmith is a platform for building production-grade LLM applications.",
-//     }),
-//   ],
-// });
-
-// console.log(reply)
 
 const retriever = vectorstore.asRetriever();
 
@@ -173,4 +114,3 @@ const result2 = await conversationalRetrievalChain.invoke({
 
 console.log(result2.answer);
 
-// console.log(reply)
